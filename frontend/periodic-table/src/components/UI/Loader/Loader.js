bc31387472a7859517718c80fe2c19a050131db0
Nitesh Sinha <niteshsinha1707@gmail.com>
2021-02-01 23:55:59 +0530

    loader added

---
+++ frontend/periodic-table/src/components/UI/Loader/Loader.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const loader = (props) => {
+  return (
+    <div class="wrap">
+      <div class="loading">
+        <div class="bounceball"></div>
+      </div>
+    </div>
+  );
+};
+
+export default loader;
