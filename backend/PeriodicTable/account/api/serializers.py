7b7a4491c8b102a0497e0f1a56a6a8e705222c50
Shagun Jain <Jshagun710@gmail.com>
2021-02-02 16:21:32 +0530

    added registration api

--- backend/PeriodicTable/account/api/serializers.py
+++ backend/PeriodicTable/account/api/serializers.py
@@ -5,4 +5,14 @@ from rest_framework.serializers import ModelSerializer
 class AccountSerializer(ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username']
+        fields = ['id', 'username', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    # def create(self, validated_data):
+    #     profile_data = validated_data.pop('profile')
+    #     password = validated_data.pop('password')
+    #     user = User(**validated_data)
+    #     user.set_password(password)
+    #     user.save()
+    #     # User.objects.create(user=user, **profile_data)
+    #     return user
--- backend/PeriodicTable/account/models.py
+++ backend/PeriodicTable/account/models.py
@@ -2,12 +2,18 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-class Profile(models.Model):
-	"""docstring for Profile"""
-	user = models.OneToOneField(User, on_delete=models.CASCADE)
-	chapter_completed = models.IntegerField(default=0, blank=True)
-	topic_completed = models.IntegerField(default=0, blank=True)
-	def __str__(self):
-		return self.user.username
 
-	
+
+class Profile(models.Model):
+    # user = models.OneToOneField(User, on_delete=models.CASCADE)
+    chapter_completed = models.IntegerField(default=0, blank=True)
+    topic_completed = models.IntegerField(default=0, blank=True)
+
+    # def __str__(self):
+    #     return self.user.username
+
+
+# @receiver(post_save, sender=settings.AUTH_USER_MODEL)
+# def create_auth_token(sender, instance=None, created=False ** kwargs):
+#     if created:
+#         Token.objects.create(user=instance)
